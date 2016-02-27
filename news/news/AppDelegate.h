eaad12127d1613037de185f79fb008bcb8fa351c
mzzs-dev01 <mzzs_dev01@foxmail.com>
2016-02-27 12:56:47 +0800

    新建项目

---
+++ news/news/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  news
+//
+//  Created by luochenxi on 16/2/27.
+//  Copyright © 2016年 lm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ news/news/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  news
+//
+//  Created by luochenxi on 16/2/27.
+//  Copyright © 2016年 lm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
